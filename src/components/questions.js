3f6a156b6401f9c93dd714b93621558587f79b49
nageshkulkarni17 <nkulkarni@qwinix.io>
2018-06-20 12:16:20 +0530

    Seperate components for questions

--- src/components/questions.js
+++ src/components/questions.js
@@ -5,9 +5,13 @@ import FormField from 'grommet/components/FormField';
 import Heading from 'grommet/components/Heading';
 import RadioButton from 'grommet/components/RadioButton';
 import Box from 'grommet/components/Box';
-import {fetchQuestions, submitAnswers} from '../redux/actions/questions';
+import {fetchQuestions} from '../redux/actions/questions';
 import Button from 'grommet/components/Button';
-import QUESTION_LIST from '../questions'
+import VERBAL from './question/verbal';
+import LOGICAL from './question/logical';
+import QUANTITATIVE from './question/quantitative';
+
+
 
 class Questions extends Component {
 	constructor(props) {
@@ -16,8 +20,8 @@ class Questions extends Component {
 			checked: null,
 			questions: {
 				logical: [],
-                quantitative: [],
-                verbal: [],
+        quantitative: [],
+        verbal: [],
 			},
 			showVerbal: props.questions.showVerbal,
 			showLogical: props.questions.showLogical,
@@ -27,7 +31,6 @@ class Questions extends Component {
 
 	componentDidMount() {
 		this.props.requestQuestions();
-		console.log('question list', QUESTION_LIST);
 	}
 
 	componentWillReceiveProps(nextProps) {
@@ -42,7 +45,6 @@ class Questions extends Component {
 			this.setState({ showQuantitative: nextProps.questions.showQuantitative })
 			this.setState({ showLogical: nextProps.questions.showLogical })
 		}
-		console.log(nextProps);
 	}
 
 
@@ -53,13 +55,13 @@ class Questions extends Component {
 		if (section_type === 'verbal') {
 			category.verbal[index].user_answer = option;
 		} else if (section_type === 'logical') {
-            category.logical[index].user_answer = option;
+      category.logical[index].user_answer = option;
 		} else if (section_type === 'quantitative') {
-           category.quantitative[index].user_answer = option;
+      category.quantitative[index].user_answer = option;
 		}
-        this.setState({
+    this.setState({
 			questions: category
-        })
+     })
 	}
 
 	  
@@ -68,219 +70,24 @@ class Questions extends Component {
 		let {userAnswers} = this.props;
 		return (
 			<div className="container mb-5">
-			{ this.state.showVerbal &&
-			  <div>
-				<Heading
-				  strong={true}
-				  uppercase={true}
-                  truncate={false}
-				  align='center'
-				  margin='medium'
-				  tag='h4'>
-                     Verbal Section
-                 </Heading>
-				 { 
-					this.state.questions.verbal.map((item, index) => {
-				    let that = this;
-				    return (
-						<div className="container mb-5" key={item.id}>
-						<Box
-						  justify='start'
-						  align='start'
-						  wrap={true}
-                          reverse={false}
-                          pad='medium'
-                          margin='small'
-                          colorIndex='light-2'
-                        >
-					      <Title truncate={false} pad='medium'>
-					        {index+1}.{item.title}
-					      </Title>
-						  {item.options.map(function (option) {
-							return (
-								<div key={option}>
-								  <Box
-						            justify='start'
-						            align='start'
-						            wrap={true}
-                                    reverse={false}
-                                    margin='small'
-                                    >
-									<RadioButton 
-									  id={option}
-								        name={option}
-										label={option}
-								        checked={item.user_answer == option}
-								        onChange={(e) => that.handleOptionChange(index,option,'verbal')}
-									   />
-									   </Box>
-							      </div>
-							   );
-						  })}
-				   </Box>
-				   </div>
-				  );
-				 })}
-				 <div>
-				 <Box
-					justify='center'
-					align='center'
-					wrap={true}
-                    reverse={false}
-                    pad='medium'
-                    margin='small'
-                 >
-				 <Button 
-					 label='Next'
-					 type='submit'
-                     accent={true}
-					 onClick={(e) => userAnswers(this.state.questions.verbal, 1)}/>
-					 </Box>
-		        	</div>
-				 </div>
+		  	{ this.state.showVerbal &&
+			  	<VERBAL
+				   verbal={this.state.questions.verbal}
+				   handleOptionChange={this.handleOptionChange.bind(this)}
+			  />
 				}
 				{ this.state.showLogical &&
-					<div>
-					  <Heading
-						strong={true}
-						uppercase={true}
-						truncate={false}
-						align='center'
-						margin='medium'
-						tag='h4'>
-						   Logical Section
-					   </Heading>
-					   { 
-						  this.state.questions.logical.map((item, index) => {
-						  let that = this;
-						  return (
-							  <div className="container mb-5" key={item.id}>
-							  <Box
-								justify='start'
-								align='start'
-								wrap={true}
-								reverse={false}
-								pad='medium'
-								margin='small'
-								colorIndex='light-2'
-							  >
-								<Title truncate={false} pad='medium'>
-								  {index+1}.{item.title}
-								</Title>
-								{item.options.map(function (option) {
-								  return (
-									  <div key={option}>
-										<Box
-										  justify='start'
-										  align='start'
-										  wrap={true}
-										  reverse={false}
-										  margin='small'
-										  >
-										  <RadioButton 
-											id={option}
-											  name={option}
-											  label={option}
-											  checked={item.user_answer == option}
-											  onChange={(e) => that.handleOptionChange(index,option,'logical')}
-											 />
-											 </Box>
-										</div>
-									 );
-								})}
-						 </Box>
-						 </div>
-						);
-					   })}
-					   <div>
-					   <Box
-						  justify='center'
-						  align='center'
-						  wrap={true}
-						  reverse={false}
-						  pad='medium'
-						  margin='small'
-					   >
-					   <Button 
-						   label='Next'
-						   type='submit'
-						   accent={true}
-						   onClick={(e) => userAnswers(this.state.questions.logical, 2)}/>
-						   </Box>
-						  </div>
-					   </div>
-					  }
-					  { this.state.showQuantitative &&
-						<div>
-						  <Heading
-							strong={true}
-							uppercase={true}
-							truncate={false}
-							align='center'
-							margin='medium'
-							tag='h4'>
-							 Quantitative Section
-						   </Heading>
-						   { 
-							  this.state.questions.quantitative.map((item, index) => {
-							  let that = this;
-							  return (
-								  <div className="container mb-5" key={item.id}>
-								  <Box
-									justify='start'
-									align='start'
-									wrap={true}
-									reverse={false}
-									pad='medium'
-									margin='small'
-									colorIndex='light-2'
-								  >
-									<Title truncate={false} pad='medium'>
-									  {index+1}.{item.title}
-									</Title>
-									{item.options.map(function (option) {
-									  return (
-										  <div key={option}>
-											<Box
-											  justify='start'
-											  align='start'
-											  wrap={true}
-											  reverse={false}
-											  margin='small'
-											  >
-											  <RadioButton 
-												id={option}
-												  name={option}
-												  label={option}
-												  checked={item.user_answer == option}
-												  onChange={(e) => that.handleOptionChange(index,option,'quantitative')}
-												 />
-												 </Box>
-											</div>
-										 );
-									})}
-							 </Box>
-							 </div>
-							);
-						   })}
-						   <div>
-						   <Box
-							  justify='center'
-							  align='center'
-							  wrap={true}
-							  reverse={false}
-							  pad='medium'
-							  margin='small'
-						   >
-						   <Button 
-							   label='Next'
-							   type='submit'
-							   accent={true}
-							   onClick={(e) => userAnswers(this.state.questions.quantitative, 3)}/>
-							   </Box>
-							  </div>
-						   </div>
-						  }
+					<LOGICAL
+					logical={this.state.questions.logical}
+			  	handleOptionChange={this.handleOptionChange.bind(this)}
+			  />
+				 }
+				{ this.state.showQuantitative &&
+					<QUANTITATIVE
+						quantitative={this.state.questions.quantitative}
+						handleOptionChange={this.handleOptionChange.bind(this)}
+						/>
+				}
 			</div>
 		)
 	}
@@ -296,9 +103,6 @@ const mapDispatchToProps = (dispatch) => ({
 	requestQuestions() {
 	  dispatch(fetchQuestions());
 	},
-	userAnswers(answers, section_number) {
-		dispatch(submitAnswers(answers, section_number));
-	  },
 });
   
 
