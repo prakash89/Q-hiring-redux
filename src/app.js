7fc58eb8bf01e3bdaf3167b194e78970f6baa133
vsanjeevagowda-s <vsanjeevagowda@qwinix.io>
2018-06-25 12:59:14 +0530

    rooturl changes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const API_END_POINT = 'http://localhost:3001/';
+const API_END_POINT = 'http://web-api-service:3001/';
 
-// const API_END_POINT = 'http://52.204.219.8/';
+// const API_END_POINT = 'http://0.0.0.0:3001/';
 
 export default API_END_POINT;
